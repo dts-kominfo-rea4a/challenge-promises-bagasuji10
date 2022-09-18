47d4816108e3a9f3a5b15f0544ef2af37d0db650
bagasuji10 <bagasuji10@gmail.com>
2022-09-18 11:10:24 +0700

    add promise add function to count emotion

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (resultEmotion) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+  const theaterPromise = theaterIXX.concat(theaterVGC)
+
+  let countresultEmotion = 0;
+  theaterPromise.forEach((result) => {
+    if (result.hasil === resultEmotion) {
+      countresultEmotion++;
+    }
+  })
+  return countresultEmotion;
+};
 
 module.exports = {
   promiseOutput,
